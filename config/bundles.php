0b467c34b7f4d4ec832c993aa5988e7bac3540a3
audreysikadi <sikadia@3il.fr>
2022-12-28 12:09:48 +0100

    Amelioration_V4

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,18 +3,66 @@
 namespace App\Controller;
 
 use App\Repository\QuestionRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(QuestionRepository $questionRepository): Response
+    public function index(PaginatorInterface $questions, Request $request, QuestionRepository $questionRepository): Response
     {
-        $questions = $questionRepository->findBy([],["createdAt" => "DESC"]);
+
+        $query = $questionRepository->findBy([],["createdAt" => "DESC"]);
+        $max=2;
+        $page=1;
+        $en = round(count($query)/$max); 
+        $array = [];
+
+        for ($i=0; $i < $en ; $i++) { 
+            # code...
+            $array[$i] = $i+1;
+        }
+    
+        $questions = $questions->paginate(
+        $query, /* query NOT result */
+        $request->query->getInt('page', 1), /*page number*/
+        $max /*limit per page*/);
+
         return $this->render('home/index.html.twig', [
             'questions' => $questions,
+            'array' => $array,
+            "page" => $page
+        ]);
+    }
+
+    #[Route('/?{page}', name: 'page')]
+    public function page(int $page,PaginatorInterface $questions, Request $request, QuestionRepository $questionRepository): Response
+    {
+
+        $query = $questionRepository->findBy([],["createdAt" => "DESC"]);
+        $max = 2;
+        $en = round(count($query)/$max); 
+        $array = [];
+
+        for ($i=0; $i < $en ; $i++) { 
+            # code...
+            $array[$i] = $i+1;
+        }
+
+      $questions = $questions->paginate(
+        $query, /* query NOT result */
+        $request->query->getInt('page', $page), /*page number*/
+        $max /*limit per page*/
+    );
+
+        return $this->render('home/index.html.twig', [
+            'questions' => $questions,
+            'array' => $array,
+            'page' => $page
         ]);
     }
 }
